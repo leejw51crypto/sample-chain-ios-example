fde18180689580aa554aaf18d7a310de04ee046e
jongwhan lee <jonghwan@crypto.com>
2020-03-16 14:53:19 +0800

    Problem: (cro-1231) no tx-building in ios example using c-bindings
    Solution: add low level tx-building api (unbond, withdraw, transfer, deposit)

--- IosSample/IosSample/MyData.swift
+++ IosSample/IosSample/MyData.swift
@@ -1,12 +1,11 @@
-//
-//  MyData.swift
-//  IosSample
-
 import Foundation
 class MyData : Codable {
+    // for rpc-json
     var tendermint: String? = "ws://localhost:26657/websocket"
     var name: String? = "a"
     var passphras: String? = ""
     var enckey: String? = ""
     var mnemonics: String? = ""
+    // low level
+    var mnemonics_lowlevel: String? = ""
 }
